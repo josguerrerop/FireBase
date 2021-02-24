22ee5ee57289f71b37dd3b26dc7c8147b6f79d6c
JOHN SEBASTIAN GUERRERO PINTO <josguerrerop@correo.udistrital.edu.co>
2021-02-24 13:34:34 -0500

    arrive

--- app/src/main/java/org/itiud/appi/inicio.java
+++ app/src/main/java/org/itiud/appi/inicio.java
@@ -5,27 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+
 
 public class inicio extends AppCompatActivity {
-    public FirebaseAuth auth = FirebaseAuth.getInstance();
-    private FirebaseUser user = auth.getCurrentUser();
     private TextView name;
-    FirebaseDatabase database ;
-    DatabaseReference player1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
-        name = findViewById(R.id.nombre);
-        if(user!=null){
-            name.setText(user.getDisplayName());
+       name = findViewById(R.id.nombre);
+       this.name.setText("Welcome!");
 
 
-        }
+             }
     }
-}
